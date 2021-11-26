24982a6110969d89c9bcfe91fde0b9cd970b0769
Julie Ng <me@julie.io>
2021-11-27 00:46:53 +0100

    start results page

---
+++ components/review/pillar-results.vue
@@ -0,0 +1,62 @@
+<template>
+	<article v-if=hasFactors>
+		<h3>{{ heading }}</h3>
+		<ul>
+			<li v-for="factor of factors" :key=factor.slug>
+				<h4>
+					<NuxtLink :to=factor.question.path>{{ factor.question.shortTitle }}</NuxtLink> -
+					{{ factor.title }}
+					<span class="score-delta">{{ symbol }}{{ factor.delta }}</span>
+				</h4>
+				<p>{{ factor.description }}</p>
+			</li>
+		</ul>
+	</article>
+</template>
+
+<script>
+export default {
+	props: {
+		factors: {
+			type: Array,
+			required: true
+		},
+
+		heading: {
+			type: String,
+			required: true
+		},
+
+		pillar: {
+			type: String,
+			required: true
+		},
+
+		symbol: {
+			type: String,
+			required: false
+		}
+	},
+
+	computed: {
+		hasFactors () {
+			console.log(`${this.pillar} ${this.heading} hasFactors()`, this.factors)
+			return this.factors.length > 0
+		}
+	}
+}
+</script>
+
+<style scoped>
+	h4 {
+		margin-bottom: 0.3em;
+	}
+
+	li p {
+		margin: 0;
+	}
+
+	.score-delta {
+		font-weight: 400;
+	}
+</style>
---
+++ pages/score.vue
@@ -0,0 +1,46 @@
+<template>
+	<div class="score-results-page">
+		<h1>Your Score</h1>
+		<section v-for="pillar of results" :key=pillar.name>
+			<h2 class="capitalize-case">{{ pillar.name }}</h2>
+
+			<review-pillar-results
+				heading="Increases ⬆️"
+				symbol="+"
+				:pillar=pillar.name
+				:factors=pillar.increases
+			></review-pillar-results>
+
+			<review-pillar-results
+				heading="Decreases ⬇️"
+				:pillar=pillar.name
+				:factors=pillar.decreases
+			></review-pillar-results>
+
+			<review-pillar-results
+				heading="Neutrals"
+				:pillar=pillar.name
+				:factors=pillar.neutrals
+			></review-pillar-results>
+		</section>
+
+		<!-- <hr>
+		<pre>{{ results }}</pre> -->
+	</div>
+</template>
+
+<script>
+export default {
+	computed: {
+		results () {
+      return this.$store.getters['decisions/analysis']
+		}
+	},
+
+	methods: {
+		hasResults (arry) {
+			return arry.length > 0
+		}
+	}
+}
+</script>
